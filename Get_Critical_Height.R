17903b702ac104dcc66a22c14468c8326f94870b
tinoswe <olivomartino@gmail.com>
2017-04-05 10:05:31 +0200

    .

---
+++ Get_Critical_Height.R
@@ -0,0 +1,132 @@
+heights <- c(1613.0, 
+             1903.0,
+             2303.0,
+             2610.0)
+HICs <- c(650.1,
+          879.5,
+          1191.0,
+          1479.9)
+
+point_label <- "I"
+
+df <- data.frame(h=heights,
+                 hic=HICs)
+
+outFile <- paste("229705_Grafici/Punto_", point_label, "_Altezza_Critica.PDF",
+                 sep="")
+
+pdf(outFile,8,6)
+
+
+par(mgp=c(3,0.45,0), 
+    tcl=-0.2, 
+    mar=c(3.0,5.3,1.1,2.1))
+plot(1e-3*df$h,
+     df$hic,
+     pch=19,
+     col="black",
+     cex=1.2,
+     xlim=c(1.5,2.8),
+     ylim=c(500,1600),
+     xlab="h [m]",
+     ylab="HIC",
+     xaxs = "i",
+     ann=FALSE,
+     axes=FALSE,
+     #title=plot_title,
+     type="n")
+
+#panel.first = c(abline(h=seq(0,2500,by=250), v=seq(0,3,by=.25), col="gray", lty=3)))
+#title(paste("Posizione:", as.character(lvl)))
+axis(side=1, 
+     at=seq(0,3,by=0.25),
+     lwd=1, 
+     cex.axis=1.2)
+axis(side=2, 
+     at=seq(0, 2500, by=250),
+     lwd=1, 
+     cex.axis=1.2,
+     las=1)
+mtext(side=1,
+      text="h [m]",
+      line=1.7,
+      cex=1.2)
+mtext(side=2,
+      text="HIC",
+      line=3.3,
+      cex=1.2)
+
+hs <- 1e-3*df$h
+hics <- df$hic
+fit_1 <- lm(hics ~ poly(hs,2,raw=T),
+            weights = 0.3*hics/100.)
+
+if (coef(fit_1)[3] < 0) {
+  fit_1 <- lm(hics ~ poly(hs,1,raw=T),
+              weights = 0.3*hics/100.)
+}
+
+x <- seq(1, 
+         3,
+         length.out = 250)
+y <- predict(fit_1,
+             data.frame(hs=x))
+lines(x,
+      y,
+      col="grey")
+
+
+points(1e-3*df$h,
+       df$hic,
+       pch=19,
+       col="black")
+
+A <- as.numeric(fit_1$coefficients[1])
+B <- as.numeric(fit_1$coefficients[2])
+C <- as.numeric(fit_1$coefficients[3])
+#curve(A + B*x + C*(x*x), 
+#      1.75, 
+#      2.5,
+#      add=TRUE,
+#      col="red")
+s1 <- Re(polyroot(c(A-1000,
+                    B,
+                    C))[1])
+yp <- predict(fit_1,
+             data.frame(hs=s1))
+
+
+
+points(s1,
+       yp,
+       pch=15,
+       col="firebrick")
+segments(s1,0,s1,yp,
+         col="firebrick",
+         lty=3,
+         lwd=2)
+segments(0,yp,s1,yp,
+         col="firebrick",
+         lty=3,
+         lwd=2)
+xtext <- s1
+ytext <- yp-20
+text(xtext,
+     ytext,
+     pos=4,
+     paste("HIC=1000, h=", round(s1,3),"m",
+           sep=""))
+
+
+plot_title <- paste("Punto ",
+                    point_label,
+                    ": altezza critica=",
+                    trunc(s1*10^1)/10^1,
+                    "m",
+                    sep="")
+title(plot_title,
+      line=-1)
+dev.off()
+
+
+
---
+++ Impact_Plots.R
@@ -0,0 +1,167 @@
+myFile <- file.choose()
+fName <- tail(strsplit(myFile,"/")[[1]],n=1)
+fPath <- sub(paste("/",fName,sep=""), "", myFile)
+
+#myFile
+#fName
+#fPath
+HIC_tmin <- 4570.39*1e-3
+HIC_tmax <- 4576.52*1e-3
+HICmax <- 1191.01
+HICinterval <- 1e3*(HIC_tmax - HIC_tmin)
+
+
+fPos <- strsplit(fName,"_")[[1]][1]
+fH <-  strsplit(strsplit(fName,"_")[[1]][2],"[.]")[[1]][1]
+
+data <- read.delim(myFile, 
+                   header = FALSE,
+                   skip = 23)
+
+t  <- data$V1 
+g  <- data$V2 
+t <- as.numeric(sub(",", ".", sub(".", "", t, fixed=TRUE), fixed=TRUE))
+g <- as.numeric(sub(",", ".", sub(".", "", g, fixed=TRUE), fixed=TRUE))
+
+df <- data.frame(t=t,
+                 g=g)
+plot(df$t,
+     df$g,
+     type="l")
+
+t_start <- 4. # this is for cutting first peak
+plot(df[t>t_start,]$t,
+     df[t>t_start,]$g,
+     type="l")
+
+g_max <- max(df[t>t_start,]$g)
+t_max <- df[t>t_start,]$t[which(df[t>t_start,]$g == g_max)]
+
+t_impact_start <- t_max - 0.01 
+t_impact_stop <- t_max + 0.011
+
+
+plot(df[t>t_impact_start & t<t_impact_stop ,]$t,
+     df[t>t_impact_start & t<t_impact_stop ,]$g,
+     type="l")
+
+#generate PDF
+outFile <- paste(fPos, "_", fH, ".", "PDF",
+                 sep="")
+
+pdf(outFile,8,6)
+p_title <- paste("Punto ",
+                 fPos,
+                 ", h=",
+                 fH,
+                 "mm",
+                 sep="")
+
+par(mgp=c(30,0.45,0), 
+    tcl=-0.4, 
+    mar=c(3.0,3.3,1.1,1.1))
+
+plot(df[t>t_impact_start & t<t_impact_stop ,]$t,
+     df[t>t_impact_start & t<t_impact_stop ,]$g,
+     type="l",
+     lwd=4,
+     col="firebrick",
+     ann=FALSE,
+     cex=1.5,
+     #xlim=c(min(df[t>t_impact_start & t<t_impact_stop ,]$t),
+     #        max(df[t>t_impact_start & t<t_impact_stop ,]$t)),
+     ylim=c(0,200),
+     axes=FALSE,
+     yaxs="i")
+ 
+axis(1,
+     pos=0,
+     at=round(seq(min(df[t>t_impact_start & t<t_impact_stop ,]$t),
+            max(df[t>t_impact_start & t<t_impact_stop ,]$t),
+            by=0.005),3),
+     lwd=1, 
+     cex.axis=1.1,
+     tck=-0.01)
+axis(2,
+    at=seq(0,
+           250,
+           by=25),
+    lwd=1, 
+    cex.axis=1.1,
+    tck=-0.01,
+    las=0)
+mtext(side=1,
+      text="Tempo [ms]",
+      line=1.7,
+      cex=1.2)
+mtext(side=2,
+      text="Accelerazione [g]",
+      line=2,
+      cex=1.2)
+
+  
+  
+#abline(v=HIC_tmin, 
+#       col="red",
+#       lty=2,
+#       lwd=2)
+#abline(v=HIC_tmax, 
+#       col="red",
+#       lty=2,
+#       lwd=2)
+
+# par(new=TRUE)
+# x_grid <- seq(from=5, to=20, by=5)
+# y_grid <- seq(from=25, to=200, by=25)
+# segments(x_grid, 0,
+#          x_grid, 300,
+#          col="lightgray",
+#          lty=2)
+# segments(0, y_grid, 
+#          20, y_grid,
+#          col="lightgray",
+#          lty=2)
+# 
+
+# par(new=TRUE)
+cord.x <- c(HIC_tmin, 
+            #df_impact[df_impact$time > HIC_tmin & df_impact$time < HIC_tmax, ]$time,
+            df[t > t_impact_start & t < t_impact_stop & t > HIC_tmin & t < HIC_tmax,]$t,
+            HIC_tmax)
+cord.y <- c(0,
+            df[t > t_impact_start & t < t_impact_stop & t > HIC_tmin & t < HIC_tmax,]$g,
+            0)
+#col2rgb("salmon")
+polycol<-rgb(red=250,
+            green=128,
+            blue=114,
+            alpha=20,
+            maxColorValue = 255)
+polygon(cord.x,
+        cord.y,
+        col=polycol,
+        border="firebrick")
+# 
+# par(new=TRUE)
+# lines(df_impact$time,
+#       df_impact$g,
+#       #type="l",
+#       lwd=4,
+#       col="firebrick",
+#       cex=1.5
+#       #xlim=c(0,20)
+# )
+
+legend("topleft",
+       horiz="FALSE",
+       c(paste("HIC = ", 
+               round(HICmax,1)), 
+         paste("dt = ", 
+               round(HICinterval,4), "ms"), 
+         paste("max(a) = ", 
+               round(max(df[t>t_impact_start & t<t_impact_stop ,]$g),1),"g")),
+       cex=1.2,
+       bty="n")
+title(p_title)
+dev.off()
+
